2bf7728ed6159984a2f3c61a6abac5bfdbb1a5aa
David Auza <davidauza01@gmail.com>
2023-08-03 09:02:25 -0500

    Add comlumns to main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,30 @@ void main() {
     home: Scaffold(
       appBar: AppBar(
         title: const Text('Hello, World!')
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              color: Colors.red
+            ),
+            child: const Text('One')
+          ),
+          Container(
+              decoration: const BoxDecoration(
+                  color: Colors.green
+              ),
+              child: const Text('Two')
+          ),
+          Container(
+              decoration: const BoxDecoration(
+                  color: Colors.blue
+              ),
+              child: const Text('Three')
+          ),
+        ],
       )
     )
   ));
