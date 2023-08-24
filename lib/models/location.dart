708de7d87ac26f58bca64d0e0e538fb2d0abeca2
David Auza <davidauza01@gmail.com>
2023-08-24 07:51:52 -0500

    Implement fetchAll and fetchByID in Location

--- lib/models/location.dart
+++ lib/models/location.dart
@@ -1,16 +1,58 @@
+import 'dart:async';
+import 'dart:convert';
 import 'package:json_annotation/json_annotation.dart';
+import 'package:http/http.dart' as http;
 import 'location_fact.dart';
+import '../endpoint.dart';
 
 part 'location.g.dart';
 
 @JsonSerializable()
 class Location {
+  final int id;
   final String name;
   final String url;
   final List<LocationFact> facts;
 
-  Location({required this.name, required this.url, required this.facts});
+  Location(
+      {required this.id,
+      required this.name,
+      required this.url,
+      required this.facts});
 
   factory Location.fromJson(Map<String, dynamic> json) =>
       _$LocationFromJson(json);
+
+  // Usually would have pagination
+  static Future<List<Location>> fetchAll() async {
+    var uri = Endpoint.uri('/locations');
+
+    final response = await http.get(uri);
+
+    if (response.statusCode != 200) {
+      throw(response.body);
+    }
+
+    List<Location> list = [];
+    for (var jsonItem in json.decode(response.body)) {
+      jsonItem['facts'] = [];
+      list.add(Location.fromJson(jsonItem));
+    }
+
+    return list;
+  }
+
+  static Future<Location> fetchByID(int id) async {
+    var uri = Endpoint.uri('/locations/$id');
+
+    final response = await http.get(uri);
+
+    if (response.statusCode != 200) {
+      throw(response.body);
+    }
+
+    final Map<String, dynamic> itemMap = json.decode(response.body);
+
+    return Location.fromJson(itemMap);
+  }
 }
--- test/unit/location_test.dart
+++ test/unit/location_test.dart
@@ -1,36 +1,17 @@
-import 'dart:convert';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:product_detail_page/models/location.dart';
 
 void main() {
-  test('test location deserialization', () {
-    const locationJSON = '''
-    {
-      "name": "Arashiyama Bamboo Grove",
-      "url": "https://cdn-images-1.medium.com/max/2000/1*vdJuSUKWl_SA9Lp-32ebnA.jpeg",
-      "facts": [
-        {
-          "title": "Summary",
-          "text": "While we could go on about the ethereal glow and seemingly endless heights of this bamboo grove on the outskirts of Kyoto, the sight's pleasures extend beyond the visual realm"
-        },
-        {
-          "title": "How to Get There",
-          "text": "Kyoto airport, with several terminals, is located 16 kilometres south of the city and is also known as Kyoto. Kyoto can also be reached by transport links from other regional airports."
-        }
-      ]
+  test('test /locations and /locations/:id', () async {
+    final locations = await Location.fetchAll();
+
+    for (var location in locations) {
+      expect(location.name, hasLength(greaterThan(0)));
+      expect(location.url, hasLength(greaterThan(0)));
+
+      final fetchedLocation = await Location.fetchByID(location.id);
+      expect(fetchedLocation.name, equals(location.name));
+      expect(fetchedLocation.url, equals(location.url));
     }
-    ''';
-
-    final locationMap = json.decode(locationJSON) as Map<String, dynamic>;
-
-    expect('Arashiyama Bamboo Grove', equals(locationMap['name']));
-
-    final location = Location.fromJson(locationMap);
-
-    expect(location.name, equals(locationMap['name']));
-    expect(location.url, equals(locationMap['url']));
-
-    expect(location.facts[0].title, matches(locationMap['facts'][0]['title']));
-    expect(location.facts[0].text, matches(locationMap['facts'][0]['text']));
   });
 }
